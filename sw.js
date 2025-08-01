6870d259e0c641ff44abdd08cffbda18bf1daed2
GitHub Action <action@github.com>
2025-08-01 07:59:24 +0000

    Auto-update app version to 1.0.123 [skip ci]

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for cur8.fun Social Network PWA
-const CACHE_NAME = 'cur8-pwa-v1.123';
-const APP_VERSION = '1.0.122'; // Questa verrà sostituita automaticamente dal workflow
-const BUILD_TIMESTAMP = '2025-07-30T12:53:37Z';
+const CACHE_NAME = 'cur8-pwa-v1.124';
+const APP_VERSION = '1.0.123'; // Questa verrà sostituita automaticamente dal workflow
+const BUILD_TIMESTAMP = '2025-08-01T07:59:24Z';
 
 const ASSETS_TO_CACHE = [
   '/',
